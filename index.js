01874f0aba61e087c170c4a9cdacd514b682323c
zluigon <luis.nmn.gonzalez@gmail.com>
2024-09-16 15:25:49 -0400

    Refactor: removed export statements

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 
 // Print out value of API key stored in .env file
 // console.log(process.env.API_KEY);
-export async function getImage(query) {
+async function getImage(query) {
   const endpoint = `https://api.giphy.com/v1/gifs/search?api_key=${process.env.API_KEY}&q=${query}&limit=25&offset=0&rating=g&lang=en&bundle=messaging_non_clips`;
 
   try {
