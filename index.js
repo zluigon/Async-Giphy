b4fe403f3c052a0c4c44bd63bcca971b3fe6c66b
zluigon <luis.nmn.gonzalez@gmail.com>
2024-09-16 14:31:57 -0400

    Update: Giphy get method complete

--- index.js
+++ index.js
@@ -1,4 +1,25 @@
-require('dotenv').config();
+require("dotenv").config();
 
 // Print out value of API key stored in .env file
-console.log(process.env.API_KEY)
+// console.log(process.env.API_KEY);
+
+async function getImage(query) {
+  const endpoint = `https://api.giphy.com/v1/gifs/search?api_key=${process.env.API_KEY}&q=${query}&limit=25&offset=0&rating=g&lang=en&bundle=messaging_non_clips`;
+
+  try {
+    let result = await fetch(endpoint);
+    let data = await result.json();
+    let arr = data.data.map((element) => element.images.original.url);
+
+    // console.log(arr)
+    return arr;
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+async function logResult() {
+  await getImage("dog");
+}
+
+logResult();
